79472d8084aa75eee1bc29b012e4a0b2a00cb6b6
Matt Liberty <matt.liberty@jetperch.com>
2024-10-06 14:51:57 -0400

    Add LIBUSB_DEVICE_STRING_COUNT case to fix "-Wswitch-enum" error on macOS.

--- libusb/os/darwin_usb.c
+++ libusb/os/darwin_usb.c
@@ -968,6 +968,7 @@ static int darwin_get_device_string(struct libusb_device *dev,
     case LIBUSB_DEVICE_STRING_SERIAL_NUMBER: 
       cf = IORegistryEntryCreateCFProperty(service, CFSTR(kUSBSerialNumberString), kCFAllocatorDefault, 0);
       break;
+    case LIBUSB_DEVICE_STRING_COUNT: /* intentional fall-through, avoid -Wswitch-enum */
     default:
       IOObjectRelease(service);
       return LIBUSB_ERROR_INVALID_PARAM;
